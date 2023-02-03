291c528c1b0a2bbb38b4b69b03d34038c4bc6539
JimPau <jpaucar789@gmail.com>
2023-02-02 19:51:24 -0500

    Se agrego un boton y una imagen

--- app/src/main/java/com/jimena/reto2/MainActivity.java
+++ app/src/main/java/com/jimena/reto2/MainActivity.java
@@ -3,13 +3,17 @@ package com.jimena.reto2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
+    public void btn_bienvenida (View view) {
+
+        Toast.makeText(MainActivity.this, "Bienvenido a este mundo ☺", Toast.LENGTH_SHORT).show();
 
 
-
+    }
 
 
     @Override
